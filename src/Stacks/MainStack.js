1843b95cfe938b0962adcb8493d43cc1c7715293
Peterson Melo <peterson.melo@nave.org.br>
2021-09-20 16:22:57 -0300

    Save

--- src/Stacks/MainStack.js
+++ src/Stacks/MainStack.js
@@ -10,7 +10,13 @@ const Stack = createStackNavigator();
 export default ()=>{
 
     return(
-<Stack.Navigator>
+<Stack.Navigator
+     initialRouteName="Preload"
+     screenOptions={{
+         headerShown: false
+    }}
+
+>
     <Stack.Screen name="Preload" component={Preload}/>
     <Stack.Screen name="SignIn" component={SignIn}/>
     <Stack.Screen name="SignUp" component={SignUp}/>
--- src/Stacks/Screens/Preload/styles.js
+++ src/Stacks/Screens/Preload/styles.js
@@ -5,6 +5,7 @@ import styled from 'styled-components';
 export const Container = styled.SafeAreaView`
 flex: 1;
 align-items: center;
-
+background-color: #63D180;
+justify-content: center;
 
 `;
